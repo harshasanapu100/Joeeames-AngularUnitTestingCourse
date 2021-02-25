6b9af88bc007d73190903caa1baba49890aed06c
Harsha Vardhan Reddy <harshasanapu100@gmail.com>
2021-02-25 22:41:57 +0530

    Initial update changes

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,10 +20,10 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'Joeeames-AngularUnitTestingCourse'`, () => {
+  it(`should have as title 'Tour of Heroes'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('Joeeames-AngularUnitTestingCourse');
+    expect(app.title).toEqual('Tour of Heroes');
   });
 
   it('should render title', () => {
