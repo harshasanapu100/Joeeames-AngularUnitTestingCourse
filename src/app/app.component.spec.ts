5a302a4c754656c797fa49af7d5ee626ba391dcb
Harsha Vardhan Reddy <harshasanapu100@gmail.com>
2021-02-26 06:49:56 +0530

    Fix the spec bug

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -25,11 +25,4 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app.title).toEqual('Tour of Heroes');
   });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('Joeeames-AngularUnitTestingCourse app is running!');
-  });
 });
